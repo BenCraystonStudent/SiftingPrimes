620a8d0ffffd13d2522f8a236db19d7dd5b305b9
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-06-16 15:24:11 +0100

    Added header file

--- SiftingPrimes/SiftingPrimes.cpp
+++ SiftingPrimes/SiftingPrimes.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
 #include <algorithm>
-
-#define RandomAccessIterator typename
-#define Integer typename
-//#define simply changes the variable to what its being defined as
-//for example, changing the arguments below from "RandomAcessIterator" and "Integer" to "typename" has the same effect as
-//defining them as above. 
-
-template <RandomAccessIterator I, Integer N>
-void mark_sieve(I first, I last, N factor)
-{
-    //assert(first != last)
-    *first = false;
-    while (last - first > factor)
-    {
-        first = first + factor;
-        *first = false;
-    }
-}
+#include "sieves.h"
 
 int main()
 {
