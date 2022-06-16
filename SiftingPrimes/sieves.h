7cf0ccd00e451b772830078d3fe1b98fc0c1d69e
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-06-16 16:02:53 +0100

    Added sift0

--- SiftingPrimes/sieves.h
+++ SiftingPrimes/sieves.h
@@ -18,3 +18,18 @@ void mark_sieve(I first, I last, N factor)
         *first = false;
     }
 }
+
+template <RandomAccessIterator I, Integer N>
+void sift0(I first, N n)
+{
+    std::fill(first, first + n, true);
+    N i(0);
+    N index_square(3);
+    // invariant: index_square = 2i^2 + 6i + 3
+    if (first[i]) //if candidate is prime
+    {
+        mark_sieve(first + index_square, first + n, i + i + 3)
+    }
+    ++i;
+    index_square = 2 * i * (i + 3) + 3;
+}
